071339cfc02c35df6d0ffbef5fe542a4e56ed894
Andrew Anderson <anderson.andrew632@gmail.com>
2025-09-01 14:27:44 -0400

    white space mattesr

--- 2_12 LAB Input and formatted output_House real estate summary.cpp
+++ 2_12 LAB Input and formatted output_House real estate summary.cpp
@@ -2,8 +2,15 @@
 using namespace std;
 
 int main() {
+   int currentPrice;
+   int lastMonthsPrice;
    
-    cout<<"HelloWorld!";
+   cin >> currentPrice;
+   cin >> lastMonthsPrice;
 
+   /* Type your code here. */
+cout << "This house is $" << currentPrice << ".";
+cout << " The change is $" << currentPrice - lastMonthsPrice << " since last month."<< endl;
+cout << "The estimated monthly mortgage is $" <<(currentPrice*0.051)/12 << "." << endl;
    return 0;
 }

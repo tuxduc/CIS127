d1b6255fc2f35a3346b379286d0e1730ac75e465
Andrew Anderson <anderson.andrew632@gmail.com>
2025-09-08 17:45:44 -0400

    error testing complete. previous errors may be related to not using in Dev mode

--- errorTestFile.cpp
+++ errorTestFile.cpp
@@ -4,18 +4,6 @@
 using namespace std;
 
 int main() {
-   int people;
-   int numPizzas;
-   double cost;
-
-   /* Type your code here. */
-   cin >> people;
-   numPizzas = ceil((people * 2)/12.0);
-   cost = 14.95 * numPizzas;
-
-   cout << "People: "<< people << endl;
-   cout << "Pizza(s) needed: " << numPizzas << endl;
-   cout << "Cost for " << numPizzas << " pizza(s): $" << fixed << setprecision(2) << cost << endl;
-   
+   cout<<"HelloWorld"<<endl;
    return 0;
 }

daff1cfcab45a110e74dd6c70e77ccd336a5ad40
Andrew Anderson <anderson.andrew632@gmail.com>
2025-09-15 16:11:36 -0400

    fuck you zybooks

--- wk4/4_26exactChangeTRYAGAIN.cpp
+++ wk4/4_26exactChangeTRYAGAIN.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
+
 
 int main() {
 /*
@@ -10,38 +12,70 @@ Dollars,Quarters,Dimes,Nickles,Pennies
     10 Dimes
     20 Nickles
     100 Pennies
-test values for Change: 24, 65, 373
+test values for Change: 24, 65, 373 (3,2,2,0,3)
+whenever there is a value greater that 0 , print that, checking for singular or plurality (ie; if more than 1 Penny, print "x Pennies")
 */
-    int Change; cout<<"how much change do you have?"; cin>>Change;
+    int Change; cin>>Change;
     int Dollars;
     int Quarters;
     int Dimes;
     int Nickels;
     int Pennies;
 
+
+if(Change==0){
+    cout<<"No change"<<endl;
+}
+else{
+//get the totals for each declination
 //Dollars
-Dollars = Change/100;
-Change = Change%100;
+Dollars = floor(Change/100);
+    Change = Change%100;
+//whenever there is a value greater that 0, print that, checking for singular or plurality (ie; if more than 1 Penny, print "x Pennies")
+if(Dollars==1){
+    cout<<Dollars<<" Dollar"<<endl;
+}
+else if(Dollars>1){
+    cout<<Dollars<<" Dollars"<<endl;
+}
 //Quartes
-Quarters = Change/25;
-Change = Change%25;
+Quarters = floor(Change/25);
+    Change = Change%25;
+if(Quarters==1){
+    cout<<Quarters<<" Quarter"<<endl;
+}
+else if(Quarters>1){
+    cout<<Quarters<<" Quarters"<<endl;
+}
 //Dimes
-Dimes = Change/10;
-Change = Change%10;
+Dimes = floor(Change/10);
+    Change = Change%10;
+if (Dimes==1){
+    cout<<Dimes<<" Dime"<<endl;
+}
+else if(Dimes>1){
+    cout<<Dimes<<" Dimes"<<endl;
+}
 //Nickels
-Nickels = Change/5;
-Change = Change%5;
+Nickels = floor(Change/5);
+    Change = Change%5;
+if (Nickels==1){
+    cout<<Nickels<<" Nickel"<<endl;
+}
+else if(Nickels>1){
+    cout<<Nickels<<" Nickels"<<endl;
+}
 //Pennies
-Pennies = Change/1;
-Change = Change%1;
+Pennies = floor(Change/1);
+    Change = Change%1;
+if (Pennies==1){
+    cout<<Pennies<<" Penny"<<endl;
+}
+else if(Pennies>1){
+    cout<<Pennies<<" Pennies"<<endl;
+}
+}
 
-cout<<Dollars<<endl;
-cout<<Quarters<<endl;
-cout<<Dimes<<endl;
-cout<<Nickels<<endl;
-cout<<Pennies<<endl;
-
-cout<<Change<<endl; //get the totals for each declination adn then if that values = 0, dont print it
 
    return 0;
 }

613f16d540c997f4b51027297aad4260c876e7ca
Andrew Anderson <anderson.andrew632@gmail.com>
2025-09-22 16:24:28 -0400

    staring lab 3

---
+++ wek5/lab5_27bruteForceEquationSolver.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a; cin<<a;
+    int b; cin<<b;
+    int c; cin<<c;
+    int d; cin<<d;
+    int e; cin<<e;
+    int f; cin<<f;
+   
+    for(x; ){
+        for(){
+
+        }
+    }
+
+   return 0;
+}

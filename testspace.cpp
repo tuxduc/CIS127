357df046c0c82452241c4416fba5b3ebca36f772
Andrew Anderson <anderson.andrew632@gmail.com>
2025-09-26 17:01:55 -0400

    test space worked!

--- testspace.cpp
+++ testspace.cpp
@@ -8,23 +8,17 @@ int main() {
    int a,b,c;cin>>a>>b>>c;
    int x,y;
    bool solutionFound=false;
+
    for(x=0; x<=5; x++){
     for(y=0; y<=5; y++){
         int equation=(a*x)+(b*y); //iterate over this equation
         if(equation==c){
-            solutionFound=true;
-            break;
-        }
-        else{
-            continue;
+            cout<<"Success!"<<endl<<"x= "<<x<<", y= "<<y;
+            return 0;
         }
     }
 }
-if(solutionFound==true){
-    cout<<"Success!"<<"x= "<<x<<", y= "<<y;
-}
-else{
     cout<<"No solution found.";
-}
+
    return 0;
 }

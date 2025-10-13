2f1c9923cb40841321aac3ec8b5fe7c29a06a50f
Andrew Anderson <anderson.andrew632@gmail.com>
2025-10-13 13:07:29 -0400

    good so far

---
+++ wk8/825LABswappingvariables.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+//goal: swap 1 & 2, adn 3 & 4
+
+/* Define your function here */ 
+void SwapValues(int& userVal1, int& userVal2, int& userVal3, int& userVal4){
+
+}
+
+int main() {
+   /* Type your code here. Your code must call the function.  */
+int userVal1; cin>>userVal1;
+int userVal2; cin>>userVal2;
+int userVal3; cin>>userVal3;
+int userVal4; cin>>userVal4;
+
+
+cout<<userVal1<<" "<<userVal2<<" "<<userVal3<<" "<<userVal4<<endl;
+   return 0;
+}

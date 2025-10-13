c5944ffc0c84d91f0ad592992709edb3ebb96804
Andrew Anderson <anderson.andrew632@gmail.com>
2025-10-13 13:09:09 -0400

    good check

--- wk8/825LABswappingvariables.cpp
+++ wk8/825LABswappingvariables.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 using namespace std;
 
-//goal: swap 1 & 2, adn 3 & 4
+//goal: swap 1 & 2, and 3 & 4, then print.
 
 /* Define your function here */ 
 void SwapValues(int& userVal1, int& userVal2, int& userVal3, int& userVal4){
-
+cout<<"Hello Dolly!";
 }
 
 int main() {
@@ -15,6 +15,7 @@ int userVal2; cin>>userVal2;
 int userVal3; cin>>userVal3;
 int userVal4; cin>>userVal4;
 
+SwapValues(userVal1,userVal2,userVal3,userVal4);
 
 cout<<userVal1<<" "<<userVal2<<" "<<userVal3<<" "<<userVal4<<endl;
    return 0;

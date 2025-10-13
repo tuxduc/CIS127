a7d99bc20be614ae4f7ac6edd2830e982857f6c2
Andrew Anderson <anderson.andrew632@gmail.com>
2025-10-13 14:12:29 -0400

    i live iterated strings in my iterated string hole

---
+++ wk8/836LABContactList.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+//goal: print the phone number of the desired contact
+
+/* Define your function here */
+string GetPhoneNumber(vector<string> nameVec, vector<string> phoneNumberVec, string contactName){
+string result;
+//iterate through nameVec. If contact is found, assigncontact
+return result;
+}
+
+int main() {
+   /* Type your code here */
+int wordpairs; cin>>wordpairs;
+string contactList; cin>>contactList;
+string contactName; cin>>contactName;
+
+vector <string> nameVec;//create nameVec
+
+vector <string> phoneNumberVec;//create phoneNumberVec
+
+string contactNumber=GetPhoneNumber(nameVec, phoneNumberVec, contactName);
+
+cout<<contactNumber<<endl;
+cout<<contactList;
+   return 0;
+}
